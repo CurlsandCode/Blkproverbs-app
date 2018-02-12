151482cff179a5411955f1daf3d9dc737bbb96ce
Monika <monika.m.williams@gmail.com>
2018-02-12 23:30:18 +0900

    fixed typo in About

--- blkproverbs-app-client/src/components/About.js
+++ blkproverbs-app-client/src/components/About.js
@@ -4,8 +4,7 @@ const About = () => {
   return (
     <div className='About'>
       <h1>About Blkproverbs</h1>
-      <p>Blkproverbs is a more than just another random quote machine, it is a celebration of all the Beauty, Love, and Knowledge in being BLK.
-		  All the credited for curating all the proverbs found in this application goes to the <a href="https://blkproverbs.tumblr.com" target="_blank" rel="noopener noreferrer"> Blkproverbs tumblr page</a> , which was created my an amazing woman name Diane. 
+      <p>Blkproverbs is a more than just another random quote machine, it is a celebration of all the Beauty, Love, and Knowledge in being BLK.All the credit for curating all the proverbs found in this application goes to the <a href="https://blkproverbs.tumblr.com" target="_blank" rel="noopener noreferrer"> Blkproverbs tumblr page</a> , which was created my an amazing woman name Diane. 
 		  </p>
     </div>
   )

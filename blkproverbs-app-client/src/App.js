eb5ae75c1708853ac2d1c583bc266a26bf0943a7
Monika <monika.m.williams@gmail.com>
2018-02-12 18:33:44 +0900

    added react routes for easy navigation

--- blkproverbs-app-client/src/App.js
+++ blkproverbs-app-client/src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Footer from './components/Footer';
+
+
+
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+			<header className="App-header">
        </header>
-        <h1 className="App-title">Welcome to Random Proverb Generator</h1>
+        <Router>
+          <div>
+            <Navbar />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </div>
+        </Router>
+        <Footer />
       </div>
     );
   }

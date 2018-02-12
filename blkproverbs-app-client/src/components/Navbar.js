367910587866d28fb1524f071f6a58f716ac39a9
Monika <monika.m.williams@gmail.com>
2018-02-12 18:37:27 +0900

    made edit to the name of the navbar file

---
+++ blkproverbs-app-client/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className="Nav">
+      <ul>
+        <li className="HomeItem"><NavLink to='/' header>BlkProverbs</NavLink></li>
+        <li><NavLink to='/proverbs' name='Proverbs'> Random Proverb</NavLink></li>
+        <li><NavLink to='/about' name='About'>About</NavLink></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar;

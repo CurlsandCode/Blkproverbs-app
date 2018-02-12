53499f6910fb556623f949072a4f53d97f279530
Monika <monika.m.williams@gmail.com>
2018-02-12 18:35:54 +0900

    added content to about page

---
+++ blkproverbs-app-client/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='About'>
+      <h1>About Blkproverbs</h1>
+      <p>Blkproverbs is a more than just another random quote machine, it is a celebration of all the Beauty, Love, and Knowledge in being BLK.
+		  All the credited for curating all the proverbs found in this application goes to the <a href="https://blkproverbs.tumblr.com" target="_blank" rel="noopener noreferrer"> Blkproverbs tumblr page</a> , which was created my an amazing woman name Diane. 
+		  </p>
+    </div>
+  )
+}
+
+export default About;

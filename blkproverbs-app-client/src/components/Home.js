8bcfedfa16c90a3210d9959735af73a6d3c320ac
Monika <monika.m.williams@gmail.com>
2018-02-12 18:51:41 +0900

    added content to Home

---
+++ blkproverbs-app-client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='Home'>
+      <h1>Welcome to Blkproverbs!</h1>
+      <p>This app allows you to randomly generate a well-known black proverb and share it with friends as a tweet.</p>
+    </div>
+  )
+}
+
+export default Home;

ea14682d9400cf5a028658dcde27d0ec2b4e93a2
Monika <monika.m.williams@gmail.com>
2018-02-12 18:34:49 +0900

    added content to footer

---
+++ blkproverbs-app-client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+      <p> Created By<a href="https://github.com/curlsandcode/blkproverbs-app">Monika Williams</a>&#169;2018</p>
+    </div>
+  )
+}
+
+export default Footer;

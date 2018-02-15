a3dc632c480f8a9ea4580eee675e727818de9218
Monika <monika.m.williams@gmail.com>
2018-02-16 01:11:16 +0900

    changed classnames or styling

--- blkproverbs-app-client/src/components/ProverbText.js
+++ blkproverbs-app-client/src/components/ProverbText.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 export const ProverbText = ({ proverb, meaning, source}) => (
   <div>
-    <p className="text-justify">{proverb}</p>
+	  <h1 className="title">
+	   <span className="icon is-medium">
+	    <i className="fa fa-quote-left" />
+	   </span>
+     {proverb}
+     <span className="icon is-medium">
+	    <i className="fa fa-quote-right" />
+	   </span>
+	 </h1>
     <p className="text-justify">{meaning}</p>
-    <p className="font-weight-bold text-right"> {source}</p>
+    <p className="text-justify"> {source}</p>
   </div>
 );

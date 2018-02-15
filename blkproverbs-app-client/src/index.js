61574d90667bece1d713e7c47d352a7dd7528487
Monika <monika.m.williams@gmail.com>
2018-02-15 15:32:45 +0900

    moved container files into the container folder

--- blkproverbs-app-client/src/index.js
+++ blkproverbs-app-client/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App  from './App';
-
+import proverbsReducer from './reducers/proverbsReducer';
+import thunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
+//import {createLogger} from 'redux-logger'
+//const logger = createLogger({collapsed: true})
 
+const store = createStore(proverbsReducer, applyMiddleware(thunk))
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+	<Provider store={store}>
+    <App />
+   </Provider>, document.getElementById('root'));
 registerServiceWorker();

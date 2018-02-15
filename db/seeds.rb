2e73de8f1d17669891aadfb08ff6e98cb06c80e5
Monika <monika.m.williams@gmail.com>
2018-02-15 17:47:21 +0900

    made changes to seed data and reseeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,269 +5,269 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
- 
+ Proverb.delete_all
 
 Proverb.create!(
       [
        {
       content: "That person or action makes my ass itch.",
-      meaning: "-said to describe something or someone annoying or irritating",
-      source: "blkproverbs tumblr" 
+      meaning: "-Said to describe something or someone annoying or irritating.",
+      source: "Blkproverbs Tumblr" 
     }, 
     {
       content: "...tryna make a dollar out of 15 cents",
-      meaning: "-said to indicate that someone is over-dramatizing a situation or bragging about something insignificant. ",
-      source: "blkproverbs tumblr" 
+      meaning: "-Said to indicate that someone is over-dramatizing a situation or bragging about something insignificant. ",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "You don’t have to go home, but you can’t stay here!",
-      meaning: "-used after a party starts wrapping up. ",
-      source: "blkproverbs tumblr" 
+      meaning: "-sed after a party starts wrapping up. ",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "A hard head, make a soft behind",
       meaning: "-usually said to warn a child that not obeying their parents will result in a whoopin.",
-      source: "blkproverbs tumblr" 
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "You’re doing the most!",
-      meaning: "-often said in response to a person who the user feels is simply doing something BOTH unnecessary and detrimental. ",
-      source: "blkproverbs tumblr" 
+      meaning: "-Often said in response to a person who the user feels is simply doing something BOTH unnecessary and detrimental. ",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "No shit Sherlock!",
-      meaning: "-you just said something really obvious.  ",
-      source: "blkproverbs tumblr" 
+      meaning: "-Uou just said something really obvious.  ",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Don’t shit under a tree, because one day it will get hot and you will need some shade.",
-      meaning: "-a warning against turning on someone who supported you in the past",
-      source: "blkproverbs tumblr" 
+      meaning: "-A warning against turning on someone who supported you in the past.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Stop crying before I give you something to cry about!",
-      meaning: "-a statement used when someone does, in fact, have something to cry about, because their mother just accosted them physically",
-      source: "blkproverbs tumblr" 
+      meaning: "-A statement used when someone does, in fact, have something to cry about, because their mother just accosted them physically.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "I may have been born at night but I wasn’t born last night.",
-      meaning: "-usually mentioned when someone is trying to get over. ",
-      source: "blkproverbs tumblr" 
+      meaning: "-Usually mentioned when someone is trying to get over. ",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "I am blessed and highly favored.",
-      meaning: "-used to signify that the Lord has been very good to you.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Used to signify that the Lord has been very good to you.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "No shade, but…",
-      meaning: "-used in a way so that you can say something offensive to someone without getting called out for it. Similar to “No offense.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Used to something offensive to someone without getting called out for it. Similar to “No offense.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Where they do that at?",
-      meaning: "-used when expressing disapproval of an action or behavior.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Used when expressing disapproval of an action or behavior.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "A hit dog will holla!",
-      meaning: "-a person guilty of a charge, will be the first to attempt to defend themselves.",
-      source: "blkproverbs tumblr" 
+      meaning: "-A person guilty of a charge, will be the first to attempt to defend themselves.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Aint got a pot to piss in or a window to throw it out.",
-      meaning: "-usually said to a person, especially a child, who is ungrateful or complaining about what they don’t have when they have no money to support or provide for themselves.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Usually said to a person, especially a child, who is ungrateful or complaining about what they don’t have when they have no money to support or provide for themselves.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Tell me why…",
-      meaning: "- usually preceding a long rant wherein the speaker has been wronged, slighted, or otherwise surprised with an undesirable occurrence.",
-      source: "blkproverbs tumblr" 
+      meaning: "- Usually preceding a long rant wherein the speaker has been wronged, slighted, or otherwise surprised with an undesirable occurrence.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "All up in the mustard, tryna play ketchup.",
-      meaning: "-used when one joins a conversation at the most inopportune/irrational time.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Used when one joins a conversation at the most inopportune/irrational time.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Who you finna try? (I bet it ain’t me!)",
-      meaning: "-a term used to ask: Who do you think you’re messing with?",
-      source: "blkproverbs tumblr" 
+      meaning: "-A term used to ask: Who do you think you’re messing with?",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Quit acting all brand new.",
-      meaning: "-stop pretending you’re not familiar with this situation.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Stop pretending you’re not familiar with this situation.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Don’t make me get ugly.",
-      meaning: "-usually stated as a warning to the foolish.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Usually stated as a warning to the foolish.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Keep my name out yo mouth!",
-      meaning: "-said as a warning to a shit talker, ignoring this request may result in a beat down.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Said as a warning to a shit talker, ignoring this request may result in a beat down.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "I know you aint talking!",
-      meaning: "-used to demonstrate shock towards someone having an opinion on a situation they are clearly guilty of",
-      source: "blkproverbs tumblr" 
+      meaning: "-Used to demonstrate shock towards someone having an opinion on a situation they are clearly guilty of.",
+      source: "Blkproverbs Tumblr" 
     },
     {
-      content: "Make you wanna slap yo momma",
-      meaning: "-used when something tastes or is good.",
-      source: "blkproverbs tumblr" 
+      content: "Make you wanna slap yo momma!",
+      meaning: "-Used when something tastes or is good.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Lawd, if it ain’t one thing it’s another.",
-      meaning: "-used when bad things happen back to back",
-      source: "blkproverbs tumblr" 
+      meaning: "-used when bad things happen back to back.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Who the hell you think you talkin’ to?",
-      meaning: "-generally an indicator that you have said something wrong. The only proper response is 'nobody'",
-      source: "blkproverbs tumblr" 
+      meaning: "-generally an indicator that you have said something wrong. The only proper response is 'nobody'.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Lowkey...",
-      meaning: "-a general introduction to an unpopular/unshared opinion, or that which one is reluctant to admit.",
-      source: "blkproverbs tumblr" 
+      meaning: "-A general introduction to an unpopular/unshared opinion, or that which one is reluctant to admit.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Let me find out…",
-      meaning: "-usually said before some gossip one thinks they know about, but hasn’t been told yet. Or to already insinuate one does not approve of said gossip",
-      source: "blkproverbs tumblr" 
+      meaning: "-Usually said before some gossip one thinks they know about, but hasn’t been told yet. Or to already insinuate one does not approve of said gossip",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Don’t let the smooth taste fool you.",
-      meaning: "-used when referring to a person who thinks you are unwilling or unable to defend yourself because of your looks, charms or sophisticated behavior.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Used when referring to a person who thinks you are unwilling or unable to defend yourself because of your looks, charms or sophisticated behavior.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Ain’t nothing open after midnight but legs and liquor stores.",
-      meaning: "-usually said to prove there is no good reason to be out late.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Usually said to prove there is no good reason to be out late.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "That’s my cousin.",
-      meaning: "-usually said for a person that is not your cousin, but is like family, or considered relative. May also genuinely just be a cousin.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Usually said for a person that is not your cousin, but is like family, or considered relative. May also genuinely just be a cousin.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "I brought you into this world, and I’ll take you out!",
-      meaning: "-usually said to disobedient children as a threat.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Usually said to disobedient children as a threat.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "It’s so quiet you can hear a rat piss on cotton.",
-      meaning: "-said to emphasize how quiet something is.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Said to emphasize how quiet something is.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Tore up from the floor up.",
-      meaning: "-when something is completed out of order, unacceptable.",
-      source: "blkproverbs tumblr" 
+      meaning: "-When something is completely out of order, unacceptable.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "_______ as the day is long.",
-      meaning: "-meaning obvious or unremittingly; stating something that is tied and true as, comparatively, long days",
-      source: "blkproverbs tumblr" 
+      meaning: "- Something that is obvious or unremittingly; stating something that is tied and true as, comparatively, long days.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "If you like it, I love it.",
-      meaning: "-used to describe acceptance of one’s life choices",
-      source: "blkproverbs tumblr" 
+      meaning: "-Used to describe acceptance of one’s life choices.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Tell the truth and shame the devil.",
-      meaning: "-tell the truth even though you want to hide it.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Tell the truth even though you want to hide it.",
+      source: "Blkproverbs Tumblr" 
     },
     {
-      content: "On my mama",
-      meaning: "-usually said to convey that on your mother’s life you are telling the truth.",
-      source: "blkproverbs tumblr" 
+      content: "On my mama.",
+      meaning: "-Usually said to convey that on your mother’s life you are telling the truth.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "HOW YOU GON TELL ME?",
-      meaning: "-how African Americans validate themselves/ourselves as a source",
-      source: "blkproverbs tumblr" 
+      meaning: "-How African Americans validate themselves/ourselves as a source.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "A closed mouth don’t get fed.",
       meaning: "-speak up if you want something.",
-      source: "blkproverbs tumblr" 
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "I used to walk 5 miles everyday to get to school.",
-      meaning: "-old folks responding to youngsters complaining about school to emphasize the privilege of education they enjoy today.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Old folks responding to youngsters complaining about school to emphasize the privilege of education they enjoy today.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Sweep around your own front door, before you try to sweep around mine.",
-      meaning: "-usually said when one feels the need to judge you/your life, but need to evaluate their own life/circumstances.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Usually said when one feels the need to judge you/your life, but needs to evaluate their own life/circumstances.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Real talk though.",
-      meaning: "-a statement of being in agree or an introductory statement to the realest thing you will hear all day.",
-      source: "blkproverbs tumblr" 
+      meaning: "-A statement of being in agreement or an introductory statement to the realest thing you will hear all day.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Thats my cue!",
-      meaning: "-usually said once something problematic has been said or done that will most likely end in an argument or worse.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Usually said once something problematic has been said or done that will most likely end in an argument or worse.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Quick, Fast, and In a hurry.",
-      meaning: "-a common phrase used to express how quickly one should complete an action.",
-      source: "blkproverbs tumblr" 
+      meaning: "-A common phrase used to express how quickly one should complete an action.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Beggars can’t be choosers.",
-      meaning: "-a way to say be grateful for what is given to you if and when you dont have the means to purchase things for yourself.",
-      source: "blkproverbs tumblr" 
+      meaning: "-A way to say be grateful for what is given to you if and when you dont have the means to purchase things for yourself.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "You either stay in or stay out, but you ain’t bout to be running in and out of my house!",
-      meaning: "-said to a child minutes before they are locked either in or out of the house.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Said to a child minutes before they are locked either in or out of the house.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Your hair is LAID!",
-      meaning: "-your hair is exceptionally well done.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Your hair is exceptionally well done.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Don’t cut your glory!",
-      meaning: "-because hair is often referred to as a crown of glory, this is said to discourage women from cutting their hair.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Because hair is often referred to as a crown of glory, this is said to discourage women from cutting their hair.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Can I live?",
-      meaning: "-why are you worried about what I am doing? Let me do what I want without judgement.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Why are you worried about what I am doing? Let me do what I want without judgement.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "… And?",
-      meaning: "-1.your statement is not an adequate or valid enough point/excuse thereby dismissed 2. a response indicating that your previous statement is missing crucial information ",
-      source: "blkproverbs tumblr" 
+      meaning: "-1.your statement is not an adequate or valid enough point/excuse thereby dismissed 2. a response indicating that your previous statement is missing crucial information. ",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "You ain’t never lied!",
       meaning: "-said in agreement with someone’s statement.",
-      source: "blkproverbs tumblr" 
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "You all up in the Kool-Aid, and don’t know the flavor!",
-      meaning: "-used when someone is attempting to insert their opinion into a conversation without fully knowing what is being discussed.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Used when someone is attempting to insert their opinion into a conversation without fully knowing what is being discussed.",
+      source: "Blkproverbs Tumblr" 
     },
     {
       content: "Your daddy wasn’t a glass-maker.",
-      meaning: "-usually said to emphasize that a person is blocking your view and you can not see through them.",
-      source: "blkproverbs tumblr" 
+      meaning: "-Usually said to emphasize that a person is blocking your view and you can not see through them.",
+      source: "Blkproverbs Tumblr" 
     }
    ]
   )

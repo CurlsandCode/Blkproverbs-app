a140d6dc679bfee984601fba4bd725c5a4f72003
Monika <monika.m.williams@gmail.com>
2018-02-15 15:25:01 +0900

    added reducer file

---
+++ blkproverbs-app-client/src/reducers/proverbsReducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+  proverb: null,
+	meaning: null,
+  source: null
+};
+
+export default function proverbsReducer(state = initialState, action) {
+  switch (action.type) {
+    case "GET_PROVERB":
+      return {
+        proverb: action.payload.content,
+				meaning: action.payload.meaning,
+        source: action.payload.source,
+      };
+    default:
+      return state;
+  }
+}

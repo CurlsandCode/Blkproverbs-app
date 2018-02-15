2d83cd761d3b8f31e274aff0210932d49f2d046c
Monika <monika.m.williams@gmail.com>
2018-02-15 22:55:56 +0900

    fixed typos and added bold elements for emphasis

--- blkproverbs-app-client/src/components/About.js
+++ blkproverbs-app-client/src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className='About'>
       <h1>About Blkproverbs</h1>
-      <p>Blkproverbs is a more than just another random quote machine, it is a celebration of all the Beauty, Love, and Knowledge in being BLK.All the credit for curating all the proverbs found in this application goes to the <a href="https://blkproverbs.tumblr.com" target="_blank" rel="noopener noreferrer"> Blkproverbs tumblr page</a> , which was created my an amazing woman name Diane. 
+      <p>Blkproverbs is a more than just another random quote machine, it is a celebration of all the <b>B</b>eauty, <b>L</b>ove, and <b>K</b>nowledge in being <b>BLK</b>.All the credit for curating all the proverbs found in this application goes to the <a href="https://blkproverbs.tumblr.com" target="_blank" rel="noopener noreferrer"> Blkproverbs tumblr page</a> , which was created by an amazing woman name Diane. 
 		  </p>
     </div>
   )
